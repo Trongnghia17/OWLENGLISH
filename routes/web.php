6efe612be8523c13be760694080501b0a6f09e13
dung <dungconmetheu@gmail.com>
2025-06-23 11:51:49 +0700

    tao migrate and modal

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.dashboard');
+});
+
+Route::prefix('error')->group(function () {
+    Route::get('/403', function () {
+        return view('errors.403');
+    })->name('error.403');
+    
+    Route::get('/404', function () {
+        return view('errors.404');
+    })->name('error.404');
+    
+    Route::get('/500', function () {
+        return view('errors.500');
+    })->name('error.500');
 });
